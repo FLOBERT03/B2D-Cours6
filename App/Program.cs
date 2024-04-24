85e60f1b443bee61233423be44f20ba4d827bf9b
Flobert <leshubs@aol.com>
2024-04-24 16:54:17 +0200

    Ajoutez des fichiers projet.

---
+++ App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiblioBanque;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancaire compte = new CompteBancaire("Pierre", 100, "dollars");
+            Console.WriteLine(compte.Decrire());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BiblioBanque/CompteBancaire.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioBanque
+{
+    public class CompteBancaire
+    {
+        private string titulaire; // Titulaire du compte
+        private double solde;     // Solde du compte
+        private string devise;    // Devise du compte
+
+        // Constructeur
+        public CompteBancaire(string leTitulaire, double soldeInitial, string laDevise)
+        {
+            titulaire = leTitulaire;
+            solde = soldeInitial;
+            devise = laDevise;
+        }
+
+        public string Titulaire
+        {
+            get => titulaire;
+        }
+        public double Solde
+        {
+            get => solde;
+        }
+        public string Devise
+        {
+            get => devise;
+        }
+
+        // Ajoute un montant au compte
+        public void Crediter(double montant)
+        {
+            solde = solde + montant;
+        }
+
+        // Retire un montant au compte
+        public void Debiter(double montant)
+        {
+            solde = solde - montant;
+        }
+
+        // Renvoie la description du compte
+        public string Decrire()
+        {
+            string description = "Le solde du compte de " + titulaire + " est de " + solde + " " + devise;
+            return description;
+        }
+    }
+}
